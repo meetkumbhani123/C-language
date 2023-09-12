65062a298fb44a9a1797ad7e30060de29b170ffe
meetkumbhani123 <140991661+meetkumbhani123@users.noreply.github.com>
2023-09-12 09:21:12 +0530

    Add files via upload

---
+++ lab5_BMULTIFICATIONUSINGBITWIVSE2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int n,p;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	n=n<<1;
+	printf("%d",n);
+	printf("\nenter the value of p\n");
+	scanf("%d",&p);
+	p=p>>1;
+	printf("%d",p);
+}
+
+
