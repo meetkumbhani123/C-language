d6bb7d5fb4fde11dea92ac1c5cc2720dc1a4e36c
meetkumbhani123 <140991661+meetkumbhani123@users.noreply.github.com>
2023-11-05 10:06:05 +0530

    Add files via upload

---
+++ lab15A_5.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+
+
+char a[100];
+printf("\n enter the string : ");
+gets(a);
+int length=strlen(a);
+printf(" string: %s",a);
+printf("\nstring length : %d",length);
+}
